f8018f3e9c662a3d523c35bdf22a3391e0d78af0
Gourav <gouravgunaga555@gmail.com>
2025-08-07 16:22:49 +0530

    chore:code cleanup

---
+++ components/Header-contextui.tsx
@@ -0,0 +1,67 @@
+"use client"
+import { ArrowRight } from "lucide-react"
+import { SignInButton, SignUpButton, UserButton, useUser } from "@clerk/nextjs"
+import { Button } from "./ui/button"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+const AuthUI = () => {
+  return (
+    <div className="flex items-center space-x-2 sm:space-x-4">
+      <SignInButton>
+        <Button variant={"outline"} className="text-xs sm:text-sm">
+          Sign In
+        </Button>
+      </SignInButton>
+
+      <SignUpButton>
+        <Button className="text-xs sm:text-sm">Sign up</Button>
+      </SignUpButton>
+    </div>
+  )
+}
+
+const HomeUI = () => {
+  const { user } = useUser()
+  return (
+    <>
+      <span className="text-xs sm:text-sm text-gray-600 hidden md:block">
+        Hi,
+        {user?.username || user?.emailAddresses[0].emailAddress.split("@")[0]}
+      </span>
+
+      <Link href={"/dashboard"}>
+        <Button size={"sm"} className="text-xs md:text-sm">
+          Go to Dashboard <ArrowRight />
+        </Button>
+      </Link>
+    </>
+  )
+}
+
+const DashboardUI = () => {
+  return <UserButton />
+}
+
+const HeaderContextUI = () => {
+  const { isSignedIn } = useUser()
+
+  const pathName = usePathname()
+  const isHomePage = pathName === "/"
+  const isDashBoardPage = pathName === "/dashboard"
+
+  return (
+    <>
+      {isSignedIn ? (
+        <div className="flex flex-col sm:flex-row space-y-2 items-baseline space-x-2">
+          {isHomePage && <HomeUI />}
+          {isDashBoardPage && <DashboardUI />}
+        </div>
+      ) : (
+        <AuthUI />
+      )}
+    </>
+  )
+}
+
+export default HeaderContextUI
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,7 @@
-"use client"
-
-import { ArrowRight, SquareKanban } from "lucide-react"
-import { SignInButton, SignUpButton, useUser } from "@clerk/nextjs"
-import { Button } from "./ui/button"
-import Link from "next/link"
+import { SquareKanban } from "lucide-react"
+import HeaderContextUI from "./Header-contextui"
 
 const Header = () => {
-  const { isSignedIn, user } = useUser()
   return (
     <header className="bg-white/80 border-b backdrop-blur-sm sticky z-50 top-0">
       <div className=" container py-3 sm:py-4 mx-auto flex items-center justify-between">
@@ -16,34 +11,7 @@ const Header = () => {
             Kanban Board
           </h1>
         </div>
-
-        {isSignedIn ? (
-          <div className="flex flex-col sm:flex-row space-y-2 items-baseline space-x-2">
-            <span className="text-xs sm:text-sm text-gray-600 hidden md:block">
-              Hi,
-              {user.username ||
-                user.emailAddresses[0].emailAddress.split("@")[0]}
-            </span>
-
-            <Link href={"/dashboard"}>
-              <Button size={"sm"} className="text-xs md:text-sm">
-                Go to Dashboard <ArrowRight />
-              </Button>
-            </Link>
-          </div>
-        ) : (
-          <div className="flex items-center space-x-2 sm:space-x-4">
-            <SignInButton>
-              <Button variant={"outline"} className="text-xs sm:text-sm">
-                Sign In
-              </Button>
-            </SignInButton>
-
-            <SignUpButton>
-              <Button className="text-xs sm:text-sm">Sign up</Button>
-            </SignUpButton>
-          </div>
-        )}
+        <HeaderContextUI />
       </div>
     </header>
   )
