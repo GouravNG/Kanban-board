5510bc0643414c9bdf3c81c5d06710dbe4c152e6
Gourav <gouravgunaga555@gmail.com>
2025-08-08 18:43:03 +0530

    chore:tanstack config

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,8 +1,19 @@
+"use client"
+
+import { getQueryClient } from "@/lib/queryclient/get-query-client"
 import { ClerkProvider } from "@clerk/nextjs"
+import { QueryClientProvider } from "@tanstack/react-query"
 import React from "react"
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <ClerkProvider>{children}</ClerkProvider>
+  const tanstackQueryClient = getQueryClient()
+  return (
+    <ClerkProvider>
+      <QueryClientProvider client={tanstackQueryClient}>
+        {children}
+      </QueryClientProvider>
+    </ClerkProvider>
+  )
 }
 
 export default Providers
