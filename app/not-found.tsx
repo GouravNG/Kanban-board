bdd463a079ec619b221918ca6bbc68bc95f1aa66
Gourav <gouravgunaga555@gmail.com>
2025-08-21 21:10:37 +0530

    not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+// app/not-found.tsx
+import React from "react"
+
+export default function NotFound() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "10vh" }}>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  )
+}
